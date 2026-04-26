dea6165dbcb45a4b255ff481bee31f0b8b12ac49
pizzo0 <acaceres@usm.cl>
2026-04-25 21:51:42 -0400

    generador de colores

---
+++ GeneradorDeColores/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace GeneradorDeColores;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ GeneradorDeColores/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Core.Platform;
+
+namespace GeneradorDeColores;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		SetColor();
+	}
+	Random rnd = new Random();
+
+	bool flag = false;
+
+	private void SetColor()
+	{
+		int r = (int)RedValue.Value;
+		int g = (int)GreenValue.Value;
+		int b = (int)BlueValue.Value;
+
+		var color = Color.FromRgb(r,g,b);
+
+		ContentPage.BackgroundColor = color;
+		HexCode.Text = $"#{r:X2}{g:X2}{b:X2}";
+	}
+
+	private void OnSliderValueChanged(object? sender, ValueChangedEventArgs args)
+	{
+		if (flag) return;
+		SetColor();
+	}
+
+	private void OnGenerateRandomColor(object? sender, EventArgs args)
+	{
+		flag = true;
+
+		RedValue.Value = rnd.Next(0,256);
+		GreenValue.Value = rnd.Next(0,256);
+		BlueValue.Value = rnd.Next(0,256);
+
+		flag = false;
+
+		SetColor();
+	}
+
+	private async void OnHexTapped(object? sender, EventArgs args)
+	{
+		await Clipboard.SetTextAsync(HexCode.Text);
+		await Toast.Make(
+			"Color copiado",
+			ToastDuration.Short
+		).Show();
+	}
+}
