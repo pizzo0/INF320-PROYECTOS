a631647988a92f79aa1adce662fc5e7836424222
ToonLucas22 <lucas.ignacio.morrison.osorio@gmail.com>
2026-06-18 18:29:46 -0400

    Funciones de cálculo de balance, ingresos y gastos

--- GP/Database/TransactionRepository.cs
+++ GP/Database/TransactionRepository.cs
@@ -5,8 +5,41 @@ namespace GP.Database;
 
 public class TransactionRepository(SQLiteConnection conn) : Repository<Transaction>(conn)
 {
-    // solo falta hacer el calculateBalance, calculateIncome y calculateExpenses
-    
+    public decimal CalculateBalance()
+    {
+        var income = CalculateIncome();
+        var expenses = CalculateExpenses();
+        return income - expenses;
+    }
+
+    public decimal CalculateIncome()
+    {
+        var transactions = Get();
+        decimal income = 0;
+        foreach (var transaction in transactions)
+        {
+            if (transaction.IsIncome)
+            {
+                income += transaction.Amount;
+            }
+        }
+        return income;
+    }
+
+    public decimal CalculateExpenses()
+    {
+        var transactions = Get();
+        decimal expenses = 0;
+        foreach (var transaction in transactions)
+        {
+            if (!transaction.IsIncome)
+            {
+                expenses += transaction.Amount;
+            }
+        }
+        return expenses;
+    }
+
     public override List<Transaction> Get()
     {
         return [.. _conn.Table<Transaction>().OrderByDescending(t => t.Date)];
