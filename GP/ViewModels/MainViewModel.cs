de30514c54d10b718fd09c7d5d27690983a91c12
ToonLucas22 <lucas.ignacio.morrison.osorio@gmail.com>
2026-06-18 19:14:44 -0400

    Funcion LoadBalance()

--- GP/ViewModels/MainViewModel.cs
+++ GP/ViewModels/MainViewModel.cs
@@ -12,10 +12,15 @@ public partial class MainViewModel : ObservableObject
     private readonly TransactionRepository _repo;
     public ObservableCollection<Transaction> Transactions { get; } = [];
 
+    public decimal Balance { get; set; }
+    public decimal TotalIncome { get; set; }
+    public decimal TotalExpenses { get; set; }
+
     public MainViewModel(TransactionRepository repo)
     {
         _repo = repo;
         LoadTransactions();
+        LoadBalance();
     }
 
     public void LoadTransactions()
@@ -25,9 +30,21 @@ public partial class MainViewModel : ObservableObject
         foreach (var t in list) Transactions.Add(t);
     }
 
+    public void LoadBalance()
+    {
+        var balance = _repo.CalculateBalance();
+        var income = _repo.CalculateIncome();
+        var expenses = _repo.CalculateExpenses();
+
+        Balance = balance;
+        TotalIncome = income;
+        TotalExpenses = expenses;
+    }
+
     [RelayCommand]
     public async Task GoToCreateTransaction()
     {
         await Shell.Current.GoToAsync(nameof(CreateTransactionPage));
+        LoadBalance();
     }
 }
